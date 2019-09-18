e048ee655b42bf64b0239d52d3f3d5dd6c307f01
AJ Gosling <ajgosling2@gmail.com>
2019-09-17 22:42:18 -0700

    day 11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,1 +1,23 @@
-console.log("hello")
+// Get our elements
+const player = document.querySelector('.player');
+const video = document.querySelector('.viewer');
+const progress = document.querySelector('.progress');
+const progressBar = document.querySelector('.progress__filled');
+const toggle = document.querySelector('.toggle');
+const skipButtons = document.querySelector('[data-skip]');
+const ranges = document.querySelector('.player__slider');
+
+console.dir(video)
+
+// Build our functions
+function togglePlay() {
+  // video.paused ?
+
+  if(video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+// Hook up Event Listeners
