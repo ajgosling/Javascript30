9048d784dee3b66839606b0b22210bd7bd69876d
AJ Gosling <ajgosling2@gmail.com>
2019-09-18 17:22:44 -0700

    finish day 11

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -35,14 +35,20 @@ function handleRangeUpdate() {
 function handleProgress() {
   const percent = (video.currentTime / video.duration) * 100;
   progressBar.style.flexBasis = `${percent}%`;
-
 }
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+  console.log(e);
+}
+
 
 // Hook up Event Listeners
 
 player.addEventListener("click", togglePlay);
 
-toggle.addEventListener("click", togglePlay);
+// toggle.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
 video.addEventListener("timeupdate", handleProgress);
@@ -53,3 +59,10 @@ ranges.forEach(range => {
   range.addEventListener("change", handleRangeUpdate);
   range.addEventListener("mousemove", handleRangeUpdate);
 })
+
+let mousedown;
+
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => mousedown = true);
+progress.addEventListener("mouseup", () => mousedown = false);
